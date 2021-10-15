e3bc66093e053663a5358c516fab95d944749011
Björn Pettersson <89526600+zocom-bjorn-pettersson@users.noreply.github.com>
2021-10-15 10:28:21 +0200

    Skapa två metoder som overloadar varandra

--- src/MethodOverloadExample.java
+++ src/MethodOverloadExample.java
@@ -1,5 +1,22 @@
 public class MethodOverloadExample {
     public static void main(String[] args) {
+        int myIntFromMethod1 = myMethod();
+        int myIntFromMethod2 = myMethod(5);
 
+        System.out.println("myIntFromMethod: " + myIntFromMethod1);
+        System.out.println("myIntFromMethod(int add): " + myIntFromMethod2);
     }
+
+    public static int myMethod() {
+        System.out.println("myMethod()");
+        int myInt = 4;
+        return myInt;
+    }
+
+    public static int myMethod(int add) {
+        System.out.println("myMethod(int add)");
+        int myInt = 4 + add;
+        return myInt;
+    }
+
 }
